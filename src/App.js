d52555725fae4e6c47841fbd0dcfce2506bdbf8f
dhasdiel <danielhasdiel@gmail.com>
2022-05-22 13:30:53 +0300

    Add Rides and RideItem(catd)

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import { theme } from "./style/theme";
 
 import BottomAppBar from "./components/BottomAppBar";
 import UpAppBar from "./components/UpAppBar";
+import Rides from "./components/Rides/Rides";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <UpAppBar />
+        <Rides />
         <BottomAppBar />
       </ThemeProvider>
     </>
--- src/components/rides/Rides.js
+++ src/components/rides/Rides.js
@@ -1,3 +1,15 @@
-const Rides = (props) => {};
+import React from "react";
+import Grid from "@mui/material/Grid";
+import RideItem from "./RideItem";
+
+const Rides = (props) => {
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <RideItem />
+      </Grid>
+    </Grid>
+  );
+};
 
 export default Rides;
