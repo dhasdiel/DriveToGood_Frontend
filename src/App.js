2e34ea19327312b5121e8c5ddaa77379e295b0bb
Moshe Mizrachi <moshe@aka.co.il>
2022-06-12 14:10:16 +0300

    added auth, router not yet implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { Button, Typography } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./style/theme";
 
@@ -8,15 +7,34 @@ import BottomAppBar from "./components/BottomAppBar";
 import UpAppBar from "./components/UpAppBar";
 import Rides from "./components/rides/Rides";
 
+import { isSigned as userSigned } from "./services/index";
+import SignIn from "./components/sign/SignIn";
+
 function App() {
+  const [isSigned, setIsSigned] = useState(false);
+
+  // Check in first render if user is signed
+  useEffect(() => {
+    userSigned()
+      .then((res) => {
+        console.log(res.data);
+        setIsSigned(true);
+      })
+      .catch((error) => {
+        console.error(error);
+        setIsSigned(false);
+      });
+  }, []);
+
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <UpAppBar />
-        <Rides />
-        <BottomAppBar />
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      {isSigned && (
+        <>
+          <UpAppBar /> <Rides /> <BottomAppBar />
+        </>
+      )}
+      {!isSigned && <SignIn />}
+    </ThemeProvider>
   );
 }
 
--- src/services/index.js
+++ src/services/index.js
@@ -2,3 +2,69 @@
 
 import axios from "axios";
 
+// TODO: CHANGE IN PRODUCTION
+const BASEURL = "http://localhost:8000/";
+
+const authorization = () => {
+  // NOT GOOD FOR PRODUCTION! should'nt be saved in local storage
+  let JWTSTR = localStorage.getItem("JWT");
+
+  // TODO: delete it:
+  if (JWTSTR === null) JWTSTR = "aaaaaaa";
+
+  const configAuthorzeied = {
+    headers: {
+      Authorization: "Bearer " + JWTSTR,
+    },
+  };
+  return configAuthorzeied;
+};
+
+/**User - End Points
+ * -------------------------------------------------
+ */
+const USER = "user/";
+
+export const isSigned = () => {
+  const authorize = authorization();
+
+  return axios.get(BASEURL + USER + "users/me/", authorize);
+};
+
+export const signUP = (userData) => {
+  return axios.post(BASEURL + "user/sign-up", userData);
+};
+
+export const signIN = (namePass) => {
+  let formData = new FormData();
+  formData.append("username", namePass.username);
+  formData.append("password", namePass.password);
+
+  const config = {
+    headers: { "content-type": "multipart/form-data" },
+  };
+
+  // should return JWT token, best for it to be saved in memory.
+  // the return should be found in the headers (bearer scheme)
+  return axios.post(BASEURL + USER + "token", formData, config);
+};
+
+/**Drive - End Points:
+ * -------------------------------------------------------------------
+ */
+const DRIVE = "drive/";
+
+export const POSTdrive = (drive) => {
+  const authorize = authorization();
+
+  axios.post(BASEURL + DRIVE + "create", drive, authorize);
+};
+
+export const GETdrives = (longitude, latitude, skip, limit) => {
+  const authorize = authorization();
+
+  axios.get(
+    `${BASEURL}${DRIVE}drives?longitude=${longitude}&latitude=${latitude}&skip=${skip}%limit=${limit}`,
+    authorize
+  );
+};
